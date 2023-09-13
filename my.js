5479f5fa6d185b2609234c0a72b34547fc4e4e24
Sam <hangho.dev@gmail.com>
2023-09-13 14:19:53 +0700

    bai-2

---
+++ my.js
@@ -0,0 +1,13 @@
+// Viết chương trình kiểm tra tuổi của học sinh có đủ điều kiện vào lớp 10 không ( 16 tuổi là đủ điều kiện )
+//Input: 10
+//Ouput: Học sinh không đủ điều kiện vào lớp 10
+let tuoi = 10;
+let a = 16;
+let b = " >= 16";
+let c = "hoc sinh du dieu kien vao lop 10";
+let d = " hoc sinh khong du dieu kien vao lop 10";
+if (tuoi == a && b) {
+  console.log(c);
+} else {
+  console.log(d);
+}
