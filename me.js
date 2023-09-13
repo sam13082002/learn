c91e43fa7e85dec5e0fe5070213314b721213f9a
Sam <hangho.dev@gmail.com>
2023-09-13 14:46:25 +0700

    bai3

---
+++ me.js
@@ -0,0 +1,18 @@
+//3. Tìm số lớn nhất trong 3 số cho trước
+//Input: 1 2 3
+//Output: 3
+let o = 3;
+let a = 1;
+let b = 2;
+let c = 3;
+let m = " 1 la so lon nhat";
+let n = " 2 la so lon nhat";
+let v = " 3 la so lon nhat";
+if (o == a) {
+  console.log(m);
+} else if (o == b) {
+  console.log(n);
+} else o == c;
+{
+  console.log(v);
+}
